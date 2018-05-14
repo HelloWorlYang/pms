a66296497edceb4ef9869078ba5cb92e2ea8b940
lincoln <137183818@qq.com>
2018-05-14 08:56:05 +0800

    init

---
+++ pms-admin/src/test/java/com/stylefeng/guns/flowable/FlowableDbInitTest.java
@@ -0,0 +1,27 @@
+package com.stylefeng.guns.flowable;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.junit.Test;
+
+/**
+ * 初始化flowable数据库(执行前需要先在数据库创建这个数据库,下面以数据库名flowable为例)
+ *
+ * @author fengshuonan
+ * @date 2017-12-02 20:32
+ */
+public class FlowableDbInitTest {
+
+    @Test
+    public void init() {
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:mysql://rds613943t1x67vo5hw7o.mysql.rds.aliyuncs.com:3306/gdyl_pms?autoReconnect=true&useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull")
+                .setJdbcUsername("pmsuser")
+                .setJdbcPassword("VBcrmTfC2nzJqk6t")
+                .setJdbcDriver("com.mysql.jdbc.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+    }
+}
